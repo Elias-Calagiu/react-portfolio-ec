9d218926ae74c96299412eaf09c7160e89437486
Elias Calagiu <eliasc81@gmail.com>
2021-03-01 07:19:55 -0800

    Set up React Router on app.js to render about, contact, and project components with header and footer components displayed across the board

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from "react"
+import Navbar from "./components/Navbar"
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Project from "./components/Project"
+import { BrowserRouter as Router, Route} from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <Header />
+      <Navbar />
+        <Route exact path="/" component={About} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/portfolio" component={Project} />
+      <Footer />
     </div>
+  </Router>
   );
 }
 
